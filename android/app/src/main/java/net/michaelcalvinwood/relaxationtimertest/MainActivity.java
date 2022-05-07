3b8e1e12c5a8509f07b5903fec9b4dd798701af1
Michael Wood <michaelwood33311@icloud.com>
2022-05-07 13:00:29 -0400

    preparing for ios distribution

--- android/app/src/main/java/net/michaelcalvinwood/relaxationtimertest/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package net.michaelcalvinwood.relaxationtimertest;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'net.michaelcalvinwood.ionicrelaxationtimer',
   appName: 'ionic-relaxation-timer',
   webDir: 'build',
   bundledWebRuntime: false
